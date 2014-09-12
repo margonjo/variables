22ec7e9bf80c8c0e1c025479abb2140dc3151ea5
Marc Gonzalez <40756@longroad.ac.uk>
2014-09-12 15:53:10 +0100

    finished number addition programme

---
+++ number addition.py
@@ -0,0 +1,12 @@
+#Marc Gonzalez
+#12/09/14
+#Revision exercise 1
+
+number_1 = int(input("please enter your first number: "))
+number_2 = int(input("please enter your second number: "))
+number_3 = int(input("please enter your third number: "))
+number_4 = int(input("please enter your fourth number: "))
+
+answer = number_1 + number_2 + number_3 + number_4
+
+print("the total of your numbers is {0}".format(answer))
