90ff1d0e94b31cf9ec5e65e05da1753369659637
Marc Gonzalez <40756@longroad.ac.uk>
2014-09-16 10:26:45 +0100

    exercise task 2.1

---
+++ Division remainder programme.py
@@ -0,0 +1,12 @@
+#Marc Gonzalez
+#16/9/14
+#exercises 2.1
+
+number_one = int(input("Please enter your first number: "))
+number_two = int(input("Please enter your second number: "))
+
+division = number_one / number_two
+
+remainder = number_one % number_two
+
+print("The answer is {0} remainder {1}".format(division, remainder))
