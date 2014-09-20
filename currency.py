208d32c88e633d9b95e213361995f164fe83c50e
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-09-20 18:32:31 +0100

    currency improved

--- currency.py
+++ currency.py
@@ -2,7 +2,7 @@
 #18/9/14
 #Curency exchange
 
-total = int(input("Please enter the amount of money you wish to convert"))
+total = int(input("Please enter the amount of money you wish to convert: "))
 
 amount_20 = total // 20
 
@@ -18,11 +18,11 @@ remainder_5 = remainder_10 % 5
 
 amount_2 = remainder_5 // 2
 
-remiander_2 = remainder_5 % 2
+remainder_2 = remainder_5 % 2
 
 amount_1 = remainder_2
 
-print(" The minimum change needed is {0} £20 notes, {1} £10 notes, {2} £5 notes, {3} £2 coins, {4} £1 coins".format(amount_20, amount_10, amount_5, amount_2, amount1))
+print(" The minimum change needed is {0} £20 notes, {1} £10 notes, {2} £5 notes, {3} £2 coins, {4} £1 coins".format(amount_20, amount_10, amount_5, amount_2, amount_1))
             
 
         
