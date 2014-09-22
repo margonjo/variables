0f0128a13b1d9c40a17573eb86e959fc3430c5a4
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-09-22 19:02:31 +0100

    complete exercise 2.3

---
+++ weight, height unit converter.py
@@ -0,0 +1,13 @@
+#Marc Gonzalez
+#22/09/14
+#exercise 2.3
+
+height = float(input("please type in your height in inches: "))
+weight = float(input("please type in your weight in stones: "))
+
+height_centimetres = height * 2.54
+
+weight_kilograms = weight * 6.364
+
+print("your height in centimetres is {0} and your weight in kilograms is {1}".format(height_centimetres, weight_kilograms))
+                     
